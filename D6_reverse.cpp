2e3e7405dc41cd35934f6fb11c0ddd178f0b3689
SACHINkgu <sachin.2024it1201@kiet.edu>
2023-07-09 00:02:01 +0530

    Create D6_reverse.cpp

---
+++ D6_reverse.cpp
@@ -0,0 +1,38 @@
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+       
+       //base case 
+       if(head == NULL)
+       return NULL;
+
+       //reverse 1st k nodes 
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* forward = NULL;
+        int count = 0;
+
+        ListNode* cursor = head;
+        for(int i = 0; i < k; i++){
+         if(cursor == nullptr) 
+             return head;
+         cursor = cursor->next;
+    }
+
+        while(curr!=NULL && count<k){
+           forward = curr->next; 
+           curr->next = prev;
+           prev = curr;
+           curr = forward;
+           count++;
+        }
+
+        // recursion dek lega
+        if(forward != NULL){
+            head->next = reverseKGroup(forward,k);
+        }
+        
+        return prev;
+    }
+};
