0a16dc2c90489648dc68fc1ba1d801be44a73ba6
SACHINkgu <sachin.2024it1201@kiet.edu>
2023-07-20 22:18:31 +0530

    Create maxarea.cpp

---
+++ D14/maxarea.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+int largestarea(int arr[], int n) {
+  int maxArea = 0;
+  for (int i = 0; i < n; i++) {
+    int minHeight = INT_MAX;
+    for (int j = i; j < n; j++) {
+      minHeight = min(minHeight, arr[j]);
+      maxArea = max(maxArea, minHeight * (j - i + 1));
+    }
+  }
+  return maxArea;
+}
