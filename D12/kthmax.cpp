232ff1438f20e0e1886ad6f260bc9d469dec40d0
SACHINkgu <sachin.2024it1201@kiet.edu>
2023-07-20 22:03:29 +0530

    Create kthmax.cpp

---
+++ D12/kthmax.cpp
@@ -0,0 +1,9 @@
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        const int n = nums.size();
+        for(int i=0;i<n;++i){pq.push(nums[i]);}
+        while(--k){
+            pq.pop();
+        }
+        return pq.top();
+    }
