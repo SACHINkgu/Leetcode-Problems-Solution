b248890ef809f5b635e78fb59d05426f1262d166
SACHINkgu <sachin.2024it1201@kiet.edu>
2023-07-25 23:08:44 +0530

    Create moris_inorder.cpp

---
+++ D17/moris_inorder.cpp
@@ -0,0 +1,33 @@
+//Just writing the function only
+
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+vector < int > inorderTraversal(node * root) {
+  vector < int > inorder;
+
+  node * cur = root;
+  while (cur != NULL) {
+    if (cur -> left == NULL) {
+      inorder.push_back(cur -> data);
+      cur = cur -> right;
+    } else {
+      node * prev = cur -> left;
+      while (prev -> right != NULL && prev -> right != cur) {
+        prev = prev -> right;
+      }
+
+      if (prev -> right == NULL) {
+        prev -> right = cur;
+        cur = cur -> left;
+      } else {
+        prev -> right = NULL;
+        inorder.push_back(cur -> data);
+        cur = cur -> right;
+      }
+    }
+  }
+  return inorder;
+}
