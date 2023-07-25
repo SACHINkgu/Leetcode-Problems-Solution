02093475fd080a544151d2c42a08c8933da8a61b
SACHINkgu <sachin.2024it1201@kiet.edu>
2023-07-25 22:40:26 +0530

    Create inorder.cpp

---
+++ D17/inorder.cpp
@@ -0,0 +1,24 @@
+//inorder traversal of tree
+
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+void inOrderTrav(node * curr, vector < int > & inOrder) {
+  if (curr == NULL)
+    return;
+
+  inOrderTrav(curr -> left, inOrder);
+  inOrder.push_back(curr -> data);
+  inOrderTrav(curr -> right, inOrder);
+}
+
+struct node * newNode(int data) {
+  struct node * node = (struct node * ) malloc(sizeof(struct node));
+  node -> data = data;
+  node -> left = NULL;
+  node -> right = NULL;
+
+  return (node);
+}
