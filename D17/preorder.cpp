00a664eb71e8d6765f26d01fb84cb3e7a787a9d6
SACHINkgu <sachin.2024it1201@kiet.edu>
2023-07-27 16:50:21 +0530

    Update preorder.cpp

---
+++ D17/preorder.cpp
@@ -0,0 +1,37 @@
+// Note i am submitting only the soution which i have submit on coding paltform you have to apply your own main function.
+//BY SACHIN KUMAR GUPTA
+
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+vector < int > preOrderTrav(node * curr) {
+  vector < int > preOrder;
+  if (curr == NULL)
+    return preOrder;
+
+  stack < node * > s;
+  s.push(curr);
+
+  while (!s.empty()) {
+    node * topNode = s.top();
+    preOrder.push_back(topNode -> data);
+    s.pop();
+    if (topNode -> right != NULL)
+      s.push(topNode -> right);
+    if (topNode -> left != NULL)
+      s.push(topNode -> left);
+  }
+  return preOrder;
+
+}
+
+struct node * newNode(int data) {
+  struct node * node = (struct node * ) malloc(sizeof(struct node));
+  node -> data = data;
+  node -> left = NULL;
+  node -> right = NULL;
+
+  return (node);
+}
