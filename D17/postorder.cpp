5dfe92db42958b4bab0ceb1b72817058435b5fdc
SACHINkgu <sachin.2024it1201@kiet.edu>
2023-07-27 16:50:09 +0530

    Update postorder.cpp

---
+++ D17/postorder.cpp
@@ -0,0 +1,25 @@
+// Note i am submitting only the soution which i have submit on coding paltform you have to apply your own main function.
+//BY SACHIN KUMAR GUPTA
+
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+void postOrderTrav(node * curr, vector < int > & postOrder) {
+  if (curr == NULL)
+    return;
+
+  postOrderTrav(curr -> left, postOrder);
+  postOrderTrav(curr -> right, postOrder);
+  postOrder.push_back(curr -> data);
+}
+
+struct node * newNode(int data) {
+  struct node * node = (struct node * ) malloc(sizeof(struct node));
+  node -> data = data;
+  node -> left = NULL;
+  node -> right = NULL;
+
+  return (node);
+}
